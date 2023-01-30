3437e136acb2cc0d09badf1f3b825d52a837b170
Taher Jerbi <jerbi.taherr@gmail.com>
2023-01-30 22:17:58 +0100

    feat: audiences analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Quicksand } from "@next/font/google";
 import { ScrollIntoViewClientComponent } from "@/hooks/scroll-into-view";
+import AnalyticsWrapper from "@/components/analytics";
 
 const font = Quicksand();
 
@@ -17,6 +18,7 @@ export default function RootLayout({
       */}
       <head />
       <body>
+        <AnalyticsWrapper />
         <ScrollIntoViewClientComponent />
         {children}
       </body>

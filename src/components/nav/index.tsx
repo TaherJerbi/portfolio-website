13c46f504ca8a4baccd5c2ee03543f2498df0fd6
Taher Jerbi <jerbi.taherr@gmail.com>
2023-01-30 04:10:38 +0100

    feat: git repo link in nav

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,3 +1,6 @@
+import { GitHub } from "react-feather";
+import Button from "../button";
+
 export default function Nav() {
   return (
     <nav className="hidden md:flex justify-between md:w-3/4 md:mx-auto  pt-12 text-2xl text-brand-blue ">
@@ -12,14 +15,34 @@ export default function Nav() {
           contact
         </a>
       </div>
-      <a
-        className="hover:underline font-semibold"
-        href={"/assets/pdf/resume.pdf"}
-        target="_blank"
-        rel="noreferrer"
-      >
-        resume
-      </a>
+      <div className="flex space-x-5 items-center">
+        <a
+          className="hover:underline font-semibold"
+          href={"/assets/pdf/resume.pdf"}
+          target="_blank"
+          rel="noreferrer"
+        >
+          resume
+        </a>
+        <a
+          href="https://github.com/taherjerbi/portfolio-website"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Button
+            fontSize="text-sm"
+            className="rounded-full !text-brand-blue"
+            bg="beige"
+            px="px-3"
+            py="py-3"
+            flat
+            hoverShadow
+            shadow="1"
+          >
+            <GitHub className="w-6 h-6 stroke-2" />
+          </Button>
+        </a>
+      </div>
     </nav>
   );
 }

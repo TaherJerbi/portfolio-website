160151037bd13876af7b5be2f573947460743779
Taher Jerbi <jerbi.taherr@gmail.com>
2023-01-30 17:01:51 +0100

    fix: link

--- src/components/section/presentation/index.tsx
+++ src/components/section/presentation/index.tsx
@@ -1,6 +1,7 @@
 import Button from "@/components/button";
 import ClientOnly from "@/components/client-only";
 import DragWindow from "@/components/draggable-nav";
+import { ElementLink } from "@/components/element-link";
 import TriangeDown from "@/components/icons/triange-down";
 import Nav from "@/components/nav";
 import Image from "next/image";
@@ -20,15 +21,15 @@ export default function PresentationSection() {
           </div>
         </div>
       </div>
-      <a
-        href="#about"
+      <ElementLink
+        id="about"
         className="absolute bottom-5 left-0 w-full flex justify-center cursor-pointer hover:underline"
       >
         <div className="flex items-center space-x-2 py-2">
           <TriangeDown className="animate-bounce drop-shadow-brand-xs hover:drop-shadow-brand-sm transition 0.2s ease-in-out" />
           <p className="text-lg text-brand-blue pb-2 font-bold">Explore</p>
         </div>
-      </a>
+      </ElementLink>
     </div>
   );
 }
